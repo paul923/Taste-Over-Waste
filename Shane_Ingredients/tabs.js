9cd8b4e12c7086c9b8ca9d3c3e443066fb467a82
shane feng <shanevi29@gmail.com>
2019-03-21 20:25:42 -0700

    update

---
+++ Shane_Ingredients/tabs.js
@@ -0,0 +1,18 @@
+var tabButtons = document.querySelectorAll("button");
+var tabPanel = document.querySelectorAll(".tabPanel");
+
+function showPanel(panelIndex, colorCode) {
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.color = "white";
+
+  tabPanel.forEach(function (node) {
+    node.style.display = "none";
+  });
+  tabPanel[panelIndex].style.display = "block";
+  tabPanel[panelIndex].style.backgroundColor = colorCode;
+}
+showPanel(1,'#4dd2ff');
