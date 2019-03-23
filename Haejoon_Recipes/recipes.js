f561a4d304d7ef49680ce4714132037e72f4cff7
HaejoonChoi <haejoonchoi@outlook.com>
2019-03-22 22:23:15 -0700

    Cleaned the JavaScript Code

--- Haejoon_Recipes/recipes.js
+++ Haejoon_Recipes/recipes.js
@@ -5,60 +5,30 @@
     function ShowList(recipeName) {
       var recipeRef = firebase.database().ref("recipes/" + recipeName);
       $("#title b").html(recipeName);
-
-      var urlRef = recipeRef.child("video-url");
-      urlRef.once("value", function (snapshot) {
+      
+      recipeRef.once("value", function (snapshot){
         data = snapshot.val();
       }).then(function () {
         console.log(data);
-        iframe = $("<iframe></iframe>").attr("src", data).attr("frameborder", "'0'");
+
+        console.log(data["ingredients-list"]);
+          for (var ingredient in data["ingredients-list"]) {
+            var li = $("<li></li>");
+    
+            li.html(data["ingredients-list"][ingredient]["name"] + ", " + data["ingredients-list"][ingredient]["amount"]);
+            $("#ingredients").append(li);
+          }
+
+        console.log(data["recipe-steps"]);
+          for (var step in data["recipe-steps"]) {
+            var li = $("<li></li>");
+    
+            li.html(step + ". " + data["recipe-steps"][step]);
+            $("#steps").append(li);
+          }
+
+        console.log(data["video-url"]);
+        iframe = $("<iframe></iframe>").attr("src", data["video-url"]).attr("frameborder", "'0'");
         $("#iframe-container").append(iframe);
-      });
-
-      var ingreRef = recipeRef.child("ingredients-list");
-      var promise = ingreRef.once("value", function (snap) {
-        list1 = snap.val();
-      });
-
-      var stepRef = recipeRef.child("recipe-steps");
-      var promise2 = stepRef.once("value", function (snap) {
-        list2 = snap.val();
-      });
-
-
-      promise.then(function () {
-        DisplayIngredients(list1); //JSON object
-      });
-
-      promise2.then(function () {
-        DisplaySteps(list2); //JSON object
-      });
+      })
     }
-
-    function DisplayIngredients(list) {
-      console.log(list);
-
-      for (var ingredient in list) {
-        var li = $("<li></li>");
-
-        li.html(list[ingredient]["name"] + ", " + list[ingredient]["amount"]);
-        $("#ingredients").append(li);
-      }
-    }
-
-    function DisplaySteps(list) {
-      console.log(list);
-
-      for (var step in list) {
-        var li = $("<li></li>");
-
-        li.html(step + ". " + list[step]);
-        $("#steps").append(li);
-      }
-    }
-
-    //    <
-    //    iframe src = "https://www.youtube.com/embed/BGVmFZ9o_Zc"
-    //    frameborder = "0"
-    //    allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-    //    allowfullscreen > < /iframe>
