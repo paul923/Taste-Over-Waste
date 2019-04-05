69c17246e1e6791497b8d1e40fb742bee0974fee
shane feng <shanevi29@gmail.com>
2019-04-05 02:03:28 -0700

    update'

--- public/js/Fire_base.js
+++ public/js/Fire_base.js
@@ -1,24 +1,58 @@
-$(document).ready(function () {
-  ShowList("apple");
-});
+var database = firebase.database();
+var ref = database.ref('exportedINGREDIENTS');
 
+function exportData1() {
 
-function ShowList(ingredients) {
-  var ingredientsRef = firebase.database().ref("ingredients/" + ingredients);
-  $(".").html(ingredients);
-
-
-  var promise = ingredientsRef.once("value", function (snap) {
-    list = snap.val();
-  });
-
-  promise.then(function () {
-    DisplayIngredients(list);
-  });
-}
-
-function DisplayIngredients(list) {
-  console.log(list);
+  var test1 = document.getElementById('c1').textContent;
+  console.log(test1);
+  
+  var data1 = {
+    Name: test1,
+    Quantity: 1,
+    Expiration: '3 days'
+  }
+  ref.push(data1);
+  console.dir(data1);
 
 }
 
+function exportData2() {
+  
+  var test2 = document.getElementById('c2').textContent;
+  
+  var data2 = {
+    Name: test2,
+    Quantity: 1,
+    Expiration: '4 days'
+  }
+  ref.push(data2);
+
+}
+
+function exportData3() {
+
+  var test3 = document.getElementById('c3').textContent;
+  console.log(test3);
+
+  var data3 = {
+    Name: test3,
+    Quantity: 1,
+    Expiration: '5 days'
+  }
+  ref.push(data3);
+  console.dir(data3);
+}
+
+function exportData4() {
+
+  var test4 = document.getElementById('c4').textContent;
+  console.log(test4);
+
+  var data4 = {
+    Name: test4,
+    Quantity: 1,
+    Expiration: '6 days'
+  }
+  ref.push(data4);
+  console.dir(data4);
+}
