290b0d2c933392b3227ca5cbf0ea7f182a4e93a6
Paul <paulgmkim@gmail.com>
2019-04-07 15:50:43 -0700

    design

---
+++ Design test/Fridge.js
@@ -0,0 +1,15 @@
+function showPanel(panelIndex, colorCode) {
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.color = "black";
+
+  tabPanel.forEach(function (node) {
+    node.style.display = "none";
+  });
+  tabPanel[panelIndex].style.display = "block";
+}
+showPanel(1, '');
+
